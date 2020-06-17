07856f3c5d50b027f253b4153eb647ed49661462
KompTech-JA <josh.arreola@hotmail.com>
2020-06-17 18:42:11 +0100

    input boxes re-styling

--- src/screens/Account/styles.js
+++ src/screens/Account/styles.js
@@ -23,6 +23,12 @@ const styles = StyleSheet.create({
   halfBox: {
     flex: 1,
   },
+  inputContainerStyle: {
+    borderBottomWidth: 0.3,
+    borderWidth: 0.3,
+    borderColor: colors.lightestGrey,
+    marginBottom: (Dimensions.get('window').height * 0.1) / 6,
+  },
 });
 
 export default styles;
